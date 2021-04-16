49c664016ce4a20982f4e6b644b28b33326157d7
AvinashhReddy <avinash12797@gmail.com>
2021-04-16 13:44:42 +0530

    added req.body to retrieve data

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const dataSchema=require('./Model')
 const cors=require('cors')
 const app=express()
 const mongoose=require('mongoose')
+const bodyParser = require('body-parser');
 app.use(cors())
 app.use(express.json())
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 app.listen(8000,()=>{console.log("server started!")})
 var payload
 const url='mongodb+srv://licious:licious@cluster0.ua32k.mongodb.net/licious?retryWrites=true&w=majority'
@@ -15,21 +20,23 @@ const con=mongoose.connection
 con.on('open',()=>{
     console.log('connected!');
 })
+
 app.post('/login',async(req,res)=>{
+
    payload={
-    "name": req.query.name,
+    "name": req.body.name,
     "iat": Math.round((new Date()).getTime()/1000)
   }
-  let data=await dataSchema.findOneAndUpdate({"name":req.query.name},{"token":createToken(payload),"tokenCreatedTime":Math.round((new Date()).getTime()/1000)})
+  let data=await dataSchema.findOneAndUpdate({"name":req.body.name},{"token":createToken(payload),"tokenCreatedTime":Math.round((new Date()).getTime()/1000)})
   await data.save()
 
-  res.json(data)
+  res.send(data)
   })
 
  app.post('/addusers',async(req,res)=>{
   const newData=new dataSchema({
-    name:req.query.name,
-    password:req.query.password
+    name:req.body.name,
+    password:req.body.password
     
 
 })
@@ -41,24 +48,24 @@ res.send("data added!")
 
 
 app.post('/insidelogin',verify,async(req,res)=>{
-  let data= await dataSchema.find({"name":req.query.name})
+  let data= await dataSchema.find({"name":req.body.name})
   if(req.token===data[0].token){
     res.send("ACCESS GRANTED!")
   }
-  else{
+  else{   
     res.send("ACCESS DENIED!")
   }
 
 })
 
 app.post('/refreshToken',async(req,res)=>{
-  let data= await dataSchema.find({"name":req.query.name})
+  let data= await dataSchema.find({"name":req.body.name})
   let difference=Math.round((new Date()).getTime()/1000)-data[0].tokenCreatedTime
   if(difference>30){
-    data=await dataSchema.findOneAndUpdate({"name":req.query.name},{"token":createToken(payload),"tokenCreatedTime":Math.round((new Date()).getTime()/1000)})
+    data=await dataSchema.findOneAndUpdate({"name":req.body.name},{"token":createToken(payload),"tokenCreatedTime":Math.round((new Date()).getTime()/1000)})
   await data.save()
   }
-  res.json(data)
+  res.json(data[0])
   
 })
 
